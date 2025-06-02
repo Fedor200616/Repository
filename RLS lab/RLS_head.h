def1a2d899380e8ed226a08fc82495d81b23aee5
Федор Почтарев PC <po4tarev.fedor@yandex.ru>
2025-06-02 20:07:42 +0300

    RLS лаба
    Готово для принятия изначально заданных параметров

--- RLS lab/RLS_head.h
+++ RLS lab/RLS_head.h
@@ -47,7 +47,16 @@ public:
 	double d; // Дальность обнаружения
 	TTarget** target; // Массив указателей на цели
 	int target_n; // Количество целей
-	TRls(double ax, double ay, double ad) : x(ax), y(ay), d(ad) {};
-	~TRls() {};
-	virtual void peleng(double t0, double tk, double dt); // Метод для вычисления пеленга на цели
+	TRls(double ax, double ay, double ad) : x(ax), y(ay), d(ad), target(nullptr), target_n(0) {};
+	~TRls() {
+		for (int i = 0; i < target_n; i++)
+			delete target[i];
+		delete[] target;
+	}
+	
+	void TInit(int n, const double* x0, const double* y0, const double* v, const double* k, 
+		const etype* types, const double* a = nullptr);	// Метод для инициализации целей
+	virtual void peleng(double t0, double tk); // Метод для вычисления пеленга на цели
 };
+
+void my_init_plane(); // Функция для инициализации самолета
--- RLS lab/RLS_main.cpp
+++ RLS lab/RLS_main.cpp
@@ -6,5 +6,9 @@ using namespace std;
 int main(){
     std::locale::global(std::locale("ru_RU.UTF-8"));
     std::wcout.imbue(std::locale());
+
+    my_init_plane();
+
     return 0;
 }
+
