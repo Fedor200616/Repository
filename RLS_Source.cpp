5c64ad12e086def886e0997326a1abe47ca8c995
Федор Почтарев PC <po4tarev.fedor@yandex.ru>
2025-06-02 17:46:37 +0300

    Лаба РЛС
    Сделано все до реализации функции создания целей

--- RLS_Source.cpp
+++ RLS_Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "RLS_head.h"
 
 
@@ -12,6 +13,24 @@ void TMissile::move(double dt){
     y = y0 + (v + a * (dt)) * std::sin(k) * dt; 
 }
 
-void TRls::peleng(int t0, int tk) {
-    for (; t0 <= tk; t0 += delta_t);
+void TRls::peleng(double t0, double tk, double dt) {
+	std::ofstream fout("Targets.txt");
+    for (double ti = t0; t0 <= tk; ti += delta_t){
+		for (int i = 0; i < target_n; i++) {
+			target[i]->move(dt);
+
+			double distance_target = std::sqrt(std::pow(target[i]->x - x, 2) 
+                + std::pow(target[i]->y - y, 2));
+            if (distance_target < d) {
+                double bearing = std::atan2(target[i]->x - x, target[i]->y - y) * 180.0 / PI;
+                if (bearing < 0) bearing += 360.0;
+
+                // Вывод в файл для обнаруженных целей D, В
+                fout << "Время: " << ti << '\t'
+					<< " Цель: " << i << '\t'
+					<< "Дистанция: " << distance_target << '\t'
+					<< " Пеленг: " << bearing << std::endl;
+            }
+		}
+    }
 }
--- RLS_head.h
+++ RLS_head.h
@@ -1,19 +1,21 @@
 #include <iostream>
-
-const float delta_t = 0.1;
+#ifndef PI
+#define PI	3.1415
+#endif
+const double delta_t = 0.1;
 
 enum class etype {
     Aircraft,
     Missile
 };
 
-class TTarget{
+class TTarget { // Цель (родительский класс)
 public:
-    double x0, y0;
-    double v;
-    double k;
-    etype type;
-    double x, y;
+	double x0, y0; // Начальные координаты цели
+	double v; // Скорость цели
+    double k; // Курс
+	etype type; // Тип цели
+	double x, y; // Текущие координаты цели
     TTarget(double ax0, double ay0, double av, double ak, etype atype, double ax, double ay)
 	: x0(ax0), y0(ay0), v(av), k(ak), type(atype), x(ax), y(ay){};
     TTarget(){};
@@ -21,27 +23,28 @@ public:
     virtual void move(double dt) = 0;
 };
 
-class TAircraft : public TTarget{
+class TAircraft : public TTarget { // Самолет
 public:
     TAircraft(double ax0, double ay0, double av, double ak, etype atype, double ax, double ay)
 	: TTarget(ax0, ay0, av, ak, atype, ax, ay){};
     void move(double dt);
 };
 
-class TMissile : public TTarget{
+class TMissile : public TTarget { // Ракета
 public:
-    double a;
+	double a; // Ускорение ракеты
     TMissile(double ax0, double ay0, double av, double ak, etype atype, double ax, double ay, double aa)
 	: TTarget(ax0, ay0, av, ak, atype, ax, ay), a(aa){};
-    void move(double dt);
+	void move(double dt); 
 };
 
-class TRls {
+class TRls { // РЛС
 public:
-    double x, y, z;
-    double d;
-    TTarget **target;
-    int target_n;
-    TRls (double ax, double ay, double az, double ad) : x(ax), y(ay), z(az), d(ad){};
-    virtual void peleng(int t0, int tk);
+	double x, y; // Координаты РЛС
+    double d; // Дальность обнаружения цели
+	TTarget** target; // Массив указателей на цели
+	int target_n; // Количество целей
+	TRls(double ax, double ay, double ad) : x(ax), y(ay), d(ad) {}; // Конструктор класса RLS
+	~TRls() {};
+	virtual void peleng(double t0, double tk, double dt); // Метод для вычисления пеленга на цели
 };
